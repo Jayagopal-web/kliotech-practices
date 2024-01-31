3a97b4bb6b5cd0dac8284d583144e4c7f9d21e86
Jayagopal-web <jayagopal369@gmail.com>
2024-01-31 13:32:56 +0530

    Second branch

---
+++ youtube/backgrounds.js
@@ -0,0 +1,15 @@
+// ytd-rich-section-renderer{
+//     display: none;
+// }
+
+// const shorts = document.getElementsByClassName("rich-section-content");
+// document.querySelectorAll("ytd-rich-section-renderer").style.display = "none";
+// document.getElementById("content").style.display = "none";
+document.getElementById("dismissible").style.display = "none";
+
+
+console.log("Working");
+// Iterate through each element in the collection
+// for (let i = 0; i < shorts.length; i++) {
+//     shorts[i].style.display = "none";
+// }
